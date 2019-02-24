18beb38a71af9c71593b222add5b66edbbfcd1cb
Daniel Gonzalez <danielmtgonzalez@gmail.com>
2019-02-24 22:31:33 +0000

    modified messages with my name

--- scripts/messages.js
+++ scripts/messages.js
@@ -55,8 +55,8 @@ function createArticle( $message ) {
         event.preventDefault( );
         let message = {
          "user": {
-         "name": "Ramses",
-         "image": "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=25"
+         "name": "Daniel",
+         "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Firefox_Logo%2C_2017.svg/1200px-Firefox_Logo%2C_2017.svg.png"
          },
          "message": {
          "text": event.target.value,
